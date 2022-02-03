102f03ee06383ef05d7a96e544764cff3c31191d
Jonas Hecht <jonas.hecht@codecentric.de>
2022-02-03 16:45:01 +0100

    Give the EKS cluster a bit more power for all the fancy K8s workloads :D

--- eks-deployment/index.ts
+++ eks-deployment/index.ts
@@ -1,7 +1,11 @@
 import * as eks from "@pulumi/eks";
 
 // Create an EKS cluster with the default configuration.
-const cluster = new eks.Cluster("eks-for-tekton");
+const cluster = new eks.Cluster("eks-for-tekton", {
+    desiredCapacity: 3,
+    minSize: 3,
+    maxSize: 4,
+});
 
 // Export the cluster's kubeconfig.
 export const kubeconfig = cluster.kubeconfig;
